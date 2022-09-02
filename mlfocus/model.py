af86c4da1fff95eb361ca1633121df96127aaacc
Talley Lambert <talley.lambert@gmail.com>
2022-09-02 18:45:17 +0000

    update

--- mlfocus/model.py
+++ mlfocus/model.py
@@ -5,9 +5,11 @@ from torchvision import models
 class FocusModel2d(nn.Module):
     def __init__(self):
         super().__init__()
+        # num_classes=1, because our output is a single scalar
+        # weights=None, because 
         self.model = models.resnet18(weights=None, num_classes=1)
         # 7 and 2 historic to alex net
-        self.model.conv1 = nn.Conv2d(2, self.model.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.model.conv1 = nn.Conv2d(2, 64, kernel_size=7, stride=2, padding=3, bias=False)
 
     def forward(self, x):
         return self.model(x)
